a886c2f4b2dc0b34f4b3e95cb87c8205b901e010
Saara21 <pips7836@lgsstudent.org>
2022-11-09 15:21:04 -0800

    asdf

---
+++ src/main/java/frc/robot/constants/IntakeOuttakeConstants.java
@@ -0,0 +1,6 @@
+package frc.robot.constants;
+
+public class IntakeOuttakeConstants {
+    public int intakeMotorID = -1;
+    public int outtakeMotorID = -1;
+}
--- src/main/java/frc/robot/subsystems/Outtake.java
+++ src/main/java/frc/robot/subsystems/Outtake.java
@@ -1,12 +1,13 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.Constants;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Outtake extends SubsystemBase {
-    CANSparkMax motor = new CANSparkMax(-1, MotorType.kBrushless);
+    CANSparkMax motor = new CANSparkMax(Constants.intakeOuttake.outtakeMotorID, MotorType.kBrushless);
     
  public void setMotors(double power) {
     motor.set(power);
