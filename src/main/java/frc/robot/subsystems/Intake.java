bcbbd3061fc6bfddde7947af3d0459e16afe0805
Saara21 <pips7836@lgsstudent.org>
2022-11-09 15:47:45 -0800

    idle modes added

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,11 +4,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Intake extends SubsystemBase {
     CANSparkMax motor = new CANSparkMax(Constants.intakeOuttake.intakeMotorID, MotorType.kBrushless);
-    
+    public Intake() {
+        motor.setIdleMode(IdleMode.kBrake);
+    }
  public void setMotors(double power) {
     motor.set(power);
     }
--- src/main/java/frc/robot/subsystems/Outtake.java
+++ src/main/java/frc/robot/subsystems/Outtake.java
@@ -4,12 +4,15 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Outtake extends SubsystemBase {
     CANSparkMax motor = new CANSparkMax(Constants.intakeOuttake.outtakeMotorID, MotorType.kBrushless);
     public Outtake(){
-        
+
+     motor.setIdleMode(IdleMode.kBrake); 
+     
     }
  public void setMotors(double power) {
     motor.set(power);
