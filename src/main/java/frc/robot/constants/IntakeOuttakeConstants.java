2238ece8328e4ddc0fed5a9f6c41a42585c2b093
Saara21 <pips7836@lgsstudent.org>
2022-11-14 15:03:01 -0800

    intake turned to command

--- src/main/java/frc/robot/constants/IntakeOuttakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeOuttakeConstants.java
@@ -3,4 +3,5 @@ package frc.robot.constants;
 public class IntakeOuttakeConstants {
     public int intakeMotorID = -1;
     public int outtakeMotorID = -1;
+    public double intakeMotorPower = 0.1;
 }
--- src/main/java/frc/robot/controls/Driver.java
+++ src/main/java/frc/robot/controls/Driver.java
@@ -1,5 +1,7 @@
 package frc.robot.controls;
 
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.Robot;
 import frc.robot.commands.DoNothing;
 import frc.robot.constants.Constants;
 import lib.controllers.GameController;
@@ -9,7 +11,7 @@ public class Driver {
   private static GameController driver = new GameController(Constants.oi.kDriverJoy);
 
   public static void configureControls() {
-    driver.get(Button.A).whenPressed(new DoNothing());
+    driver.get(Button.A).whenPressed(new RunCommand( () -> Robot.intake.setMotors(Constants.intakeOuttake.intakeMotorPower) ) );
   }
 
 }
