73b57eccc5e2f77dd1241947d8217388cb022d98
Andrea Duan <90481687+andrduan@users.noreply.github.com>
2022-11-18 15:42:35 -0800

    entered constants

--- src/main/java/frc/robot/constants/DriveConstants.java
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -1,10 +1,28 @@
 package frc.robot.constants;
 
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+
 public class DriveConstants {
 
   public final int kLeftMotor1 = -1;
   public final int kLeftMotor2 = -1;
   public final int kRightMotor1 = -1;
   public final int kRightMotor2 = -1;
+//Pathweaver stuff below all set to 0 for now 
+  public static final double ksVolts = 0;
+  public static final double kvVoltSecondsPerMeter = 0;
+  public static final double kaVoltSecondsSquaredPerMeter = 0;
+
+  // Example value only - as above, this must be tuned for your drive!
+  public static final double kPDriveVel = 0;
   
+  public static final double kTrackwidthMeters = 0.635;
+  public static final DifferentialDriveKinematics kDriveKinematics =
+      new DifferentialDriveKinematics(kTrackwidthMeters);
+
+  public static final double kMaxSpeedMetersPerSecond = 3;
+  public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+
+   public static final double kRamseteB = 2;
+    public static final double kRamseteZeta = 0.7;
 }
--- src/main/java/frc/robot/controls/Driver.java
+++ src/main/java/frc/robot/controls/Driver.java
@@ -2,7 +2,8 @@ package frc.robot.controls;
 
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.Robot;
-import frc.robot.commands.DoNothing;
+import frc.robot.commands.DoNothing+
+;
 import frc.robot.constants.Constants;
 import lib.controllers.GameController;
 import lib.controllers.GameController.Button;
