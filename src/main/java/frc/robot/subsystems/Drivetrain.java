ac2bb863d8833c9cd3439519e7f853ad0521f9fc
Andrea Duan <90481687+andrduan@users.noreply.github.com>
2022-11-18 16:17:28 -0800

    encoders

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -9,8 +9,12 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
+import frc.robot.constants.DriveConstants;
 import frc.robot.util.MotorFactory;
 
 
@@ -20,14 +24,15 @@ public class Drivetrain extends SubsystemBase {
 
   private final WPI_TalonFX m_rightMotor1;
 
-
+  private final DifferentialDrive m_drive;
+ 
   public Drivetrain() {
     m_leftMotor1 = MotorFactory.createTalonFX(Constants.drive.kLeftMotor1);
   
     m_rightMotor1 = MotorFactory.createTalonFX(Constants.drive.kRightMotor1);
 
+    m_drive = new DifferentialDrive(m_leftMotor1, m_rightMotor1);
   }
-
   /**
    * Drives the robot using tank drive controls
    * Tank drive is slightly easier to code but less intuitive to control, so this is here as an example for now
@@ -48,5 +53,16 @@ public class Drivetrain extends SubsystemBase {
   public void arcadeDrive(double throttle, double turn) {
     m_leftMotor1.set(ControlMode.PercentOutput, throttle + turn);
     m_rightMotor1.set(ControlMode.PercentOutput, throttle - turn);
-  }
 }
+  public double getLeftEncoderValues() {
+  return m_leftMotor1.getSelectedSensorPosition();
+}
+  public double getRightEncoderValues() {
+  return m_rightMotor1.getSelectedSensorPosition();
+} 
+
+m_leftEncoder.setDistancePerPulse(DriveConstants.kEncoderDistancePerPulse);
+m_rightEncoder.setDistancePerPulse(DriveConstants.kEncoderDistancePerPulse);
+
+    }
+
